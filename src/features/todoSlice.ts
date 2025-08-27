1640883d9484c8e6cea846311ad35d817262bead
Stephen <stephennelson.dev@gmail.com>
2025-08-26 19:30:57 -0500

    Adds the todoSlice to the redux store

---
+++ src/features/todoSlice.ts
@@ -0,0 +1,47 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+type Todo = {
+  id: number;
+  todo: string;
+  completed: boolean;
+  userId: number;
+};
+
+interface TodoState {
+  url: string;
+  userId: number;
+  todos: Todo[];
+}
+
+const initialState: TodoState = {
+  url: "https://dummyjson.com/todos",
+  userId: 1,
+  todos: [],
+};
+
+const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    setTodos: (state, action: PayloadAction<Todo[]>) => {
+      state.todos = action.payload;
+    },
+    addTodo: (state, action: PayloadAction<Todo>) => {
+      state.todos.push(action.payload);
+    },
+    deleteTodo: (state, action: PayloadAction<number>) => {
+      state.todos.filter((t) => t.id !== action.payload);
+    },
+    modifyTodo: (
+      state,
+      action: PayloadAction<{ id: number; todo: string }>
+    ) => {
+      state.todos.map((t) =>
+        t.id === action.payload.id ? { ...t, todo: action.payload.todo } : t
+      );
+    },
+  },
+});
+
+export const { setTodos, addTodo, deleteTodo, modifyTodo } = todoSlice.actions;
+export default todoSlice.reducer;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appReducer from "../features/appSlice";
+import todoReducer from "../features/todoSlice";
 
 export const store = configureStore({
   reducer: {
     app: appReducer,
+    todo: todoReducer,
   },
 });
 
