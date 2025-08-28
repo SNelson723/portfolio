e3cfd8ce20bb731a69b8c3713bb00ddce43e21b2
Stephen <stephenn649@gmail.com>
2025-08-28 09:01:18 -0500

    Gives the app the toast provider to call throughout the pages

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastProvider } from "./components/toast/ToastProvider.tsx";
 
 // Pages
 import Home from "./pages/home/Home.tsx";
@@ -13,14 +14,16 @@ import YoutubeHome from "./pages/youtube/YoutubeHome.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="youtube" element={<YoutubeHome />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <ToastProvider autoClose={true} duration={3500}>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route index element={<Home />} />
+              <Route path="youtube" element={<YoutubeHome />} />
+            </Route>
+          </Routes>
+        </ToastProvider>
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
